a80d1b8c69e4a4a273e39a93aaefe00118479d5a
garrisoncameron-dotcom <garrison.cameron@gmail.com>
2026-06-23 00:06:25 -0400

    Bump cache for PDF tuning

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inspectaid-cache-v6';
+const CACHE_NAME = 'inspectaid-cache-v7';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const ASSETS = [BASE_PATH, `${BASE_PATH}index.html`];
 
