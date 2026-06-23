9e5d1435aed5e0e927950d18ccc0f1540a8d5fc6
garrisoncameron-dotcom <garrison.cameron@gmail.com>
2026-06-23 00:53:38 -0400

    Bump cache for admin editable mapper

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inspectaid-cache-v10';
+const CACHE_NAME = 'inspectaid-cache-v11';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const ASSETS = [BASE_PATH, `${BASE_PATH}index.html`];
 
