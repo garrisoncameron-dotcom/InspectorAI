ceec0292440ae7b230c5e098b23eafd4c3a44690
garrisoncameron-dotcom <garrison.cameron@gmail.com>
2026-06-25 19:14:11 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inspectaid-cache-v12';
+const CACHE_NAME = 'inspectaid-cache-v13';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const ASSETS = [BASE_PATH, `${BASE_PATH}index.html`];
 
