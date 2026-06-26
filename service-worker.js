6066940e58033a61fb0c9f57f27a3d05dbe78224
garrisoncameron-dotcom <garrison.cameron@gmail.com>
2026-06-25 21:02:12 -0400

    Bump cache for Core URL fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inspectaid-cache-v16';
+const CACHE_NAME = 'inspectaid-cache-v17';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const ASSETS = [BASE_PATH, `${BASE_PATH}index.html`];
 
